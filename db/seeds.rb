6633001bd6c5af340f22525526040fdc298f9ae3
Charles Shorter <cshorter.mc@gmail.com>
2019-08-26 20:33:20 -0400

    added seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "Clearing the database"
+Post.destroy_all
+
+puts "Database has been cleared.."
+
+puts 'Seeding database with posts..'
+
+post1 = Post.create!(title: "Post 1", content: "Content for post 1")
+post2 = Post.create!(title: "Post 2", content: "Content for post 2")
+post3 = Post.create!(title: "Post 3", content: "Content for post 3")
+post4 = Post.create!(title: "Post 4", content: "Content for post 4")
+post5 = Post.create!(title: "Post 5", content: "Content for post 5")
+post6 = Post.create!(title: "Post 6", content: "Content for post 6")
+post7 = Post.create!(title: "Post 7", content: "Content for post 7")
+
+puts 'Posts created!'
+
+puts 'The seed has been completed'
